91ae835d06384e849773cd076cd6f66b25dd6d2d
Jérémy Auzou <jerem.auz@gmail.com>
2021-05-20 23:58:22 +0200

    Added proper support for Demo (SteamVR)
    Closes #2 (Does not work with the demo)

--- BetterTcMirror/MirrorPlugin.cs
+++ BetterTcMirror/MirrorPlugin.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using BepInEx;
 using BepInEx.Configuration;
 using BetterTcMirror.Behaviours;
@@ -8,11 +9,24 @@ using UnityEngine.XR;
 
 namespace BetterTcMirror
 {
-    [BepInPlugin("org.aarcnoa.tc.bettermirror", "Better TC Mirror", "1.0.0")]
+    [BepInPlugin("org.aarcnoa.tc.bettermirror", "Better TC Mirror", "1.0.1")]
     [BepInProcess("Chronos.exe")]
     [BepInProcess("ChronosDemo.exe")]
     public class MirrorPlugin : BaseUnityPlugin
     {
+        protected string[] CameraObjectPaths
+        {
+            get
+            {
+                return Paths.ProcessName switch
+                {
+                    "Chronos" => new [] {"OpenVRCameraRig(Clone)/Camera"}, // Tokyo Chronos Steam v1.0.4
+                    "ChronosDemo" => new [] {"OpenVRCameraRig/Camera (eye)"},
+                    _ => throw new InvalidOperationException($"This is not supposed to happen - process name was {Paths.ProcessName}")
+                };
+            }
+        }
+        
         private ConfigEntry<float> fieldOfView;
         private ConfigEntry<Vector3> cameraOffset;
 
@@ -20,12 +34,13 @@ namespace BetterTcMirror
         
         private GameObject _currentMirror;
 
-        private MirrorPlugin()
+        protected MirrorPlugin()
         {
             // Configuration
             fieldOfView = Config.Bind("General", "FieldOfView", 70f, "The field of view of the mirror camera.");
             cameraOffset = Config.Bind("General", "Offset", Vector3.zero,
                 "How much to move the camera relative to the player's head.");
+            
             
             SceneManager.sceneLoaded += (scene, mode) =>
             {
@@ -63,12 +78,27 @@ namespace BetterTcMirror
             var tmp = SceneManager.GetActiveScene();
             SceneManager.SetActiveScene(targetScene);
 
-            var camObj = GameObject.Find("OpenVRCameraRig(Clone)/Camera");
+            GameObject cameraObject = null;
 
-            _currentMirror = Instantiate(camObj);
+            foreach (var path in CameraObjectPaths)
+            {
+                cameraObject = GameObject.Find(path);
+                if (cameraObject != null)
+                    break;
+            }
+
+            if (cameraObject == null)
+            {
+                Logger.LogError("Could not find the expected camera object. " +
+                                "Please report this issue at https://github.com/Jeremiidesu/BetterTcMirror with the " +
+                                "version of the game you are using! (Store, version number)");
+                yield break;
+            }
+
+            _currentMirror = Instantiate(cameraObject);
             _currentMirror.name = "BetterMirrorManager";
             var mcb = _currentMirror.AddComponent<MirrorCameraBehaviour>();
-            mcb.Init(camObj, fieldOfView.Value, cameraOffset.Value);
+            mcb.Init(cameraObject, fieldOfView.Value, cameraOffset.Value);
 
             // ...and restoring it because I'd rather be too careful than not enough
             SceneManager.SetActiveScene(tmp);
